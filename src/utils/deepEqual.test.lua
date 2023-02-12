96a93232b110f0402b1a121891bb819f3bd4fd0f
Derpius <49565664+Derpius@users.noreply.github.com>
2023-02-12 18:16:25 +0000

    Organise tests

--- src/utils/deepEqual.test.lua
+++ src/utils/deepEqual.test.lua
@@ -1,47 +1,53 @@
 local deepEqual = require("src.utils.deepEqual")
 
 describe("deepEqual", function()
-	it("should return true when shallow values are equal", function()
-		-- Given
-		local a, b = true, true
+	describe("should return true", function()
+		it("when shallow values are equal", function()
+			-- Given
+			local a, b = true, true
 
-		-- When
-		local received = deepEqual(a, b)
+			-- When
+			local received = deepEqual(a, b)
 
-		-- Then
-		expect(received).toBe(true)
+			-- Then
+			expect(received).toBe(true)
+		end)
+
+		it("when table values are deeply equal", function()
+			-- Given
+			local a, b =
+				{ a = { b = { c = true } } }, { a = { b = { c = true } } }
+
+			-- When
+			local received = deepEqual(a, b)
+
+			-- Then
+			expect(received).toBe(true)
+		end)
 	end)
 
-	it("should return false when shallow values are not equal", function()
-		-- Given
-		local a, b = true, false
+	describe("should return false", function()
+		it("when shallow values are not equal", function()
+			-- Given
+			local a, b = true, false
 
-		-- When
-		local received = deepEqual(a, b)
+			-- When
+			local received = deepEqual(a, b)
 
-		-- Then
-		expect(received).toBe(false)
-	end)
+			-- Then
+			expect(received).toBe(false)
+		end)
 
-	it("should return true when table values are deeply equal", function()
-		-- Given
-		local a, b = { a = { b = { c = true } } }, { a = { b = { c = true } } }
+		it("when table values are not deeply equal", function()
+			-- Given
+			local a, b =
+				{ a = { b = { c = true } } }, { a = { b = { c = false } } }
 
-		-- When
-		local received = deepEqual(a, b)
+			-- When
+			local received = deepEqual(a, b)
 
-		-- Then
-		expect(received).toBe(true)
-	end)
-
-	it("should return false when table values are not deeply equal", function()
-		-- Given
-		local a, b = { a = { b = { c = true } } }, { a = { b = { c = false } } }
-
-		-- When
-		local received = deepEqual(a, b)
-
-		-- Then
-		expect(received).toBe(false)
+			-- Then
+			expect(received).toBe(false)
+		end)
 	end)
 end)
