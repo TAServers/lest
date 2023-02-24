a34b8cd2d1757052d5d9b50fef6e21b191993a70
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-02-23 18:42:00 -0800

    Add `tohaveLength`, `toContain`, and `toContainEqual`

--- src/matchers/tables.lua
+++ src/matchers/tables.lua
@@ -1,13 +1,80 @@
----@type lest.Matcher
-local function toHaveLength(ctx, received, length) end
+local prettyValue = require("src.utils.prettyValue")
+local deepEqual = require("src.utils.deepEqual")
 
 ---@type lest.Matcher
-local function toContain(ctx, received, item) end
+local function toHaveLength(ctx, received, length)
+	local receivedLen = #received
+
+	return {
+		pass = receivedLen == length,
+		message = string.format(
+			"Expected %s to%shave the length of %d",
+			prettyValue(received),
+			ctx.inverted and " not " or " ",
+			length
+		),
+	}
+end
 
 ---@type lest.Matcher
-local function toContainEqual(ctx, received, item) end
+local function toContain(ctx, received, item)
+	local pass = false
+
+	if type(received) == "string" then
+		pass = received:find(item) ~= nil
+	elseif type(received) == "table" then
+		for _, value in pairs(received) do
+			if value == item then
+				pass = true
+				break
+			end
+		end
+	else
+		error("Expected to receive a string or a table")
+	end
+
+	return {
+		pass = pass,
+		message = string.format(
+			"Expected %s to%scontain %s",
+			prettyValue(received),
+			ctx.inverted and " not " or " ",
+			prettyValue(item)
+		),
+	}
+end
+
+---@type lest.Matcher
+local function toContainEqual(ctx, received, item)
+	local pass = false
+
+	if type(received) == "table" then
+		for _, value in pairs(received) do
+			if deepEqual(value, item) then
+				pass = true
+				break
+			end
+		end
+	else
+		error("Expected to receive a table")
+	end
+
+	return {
+		pass = pass,
+		message = string.format(
+			"Expected %s to%scontain %s with deep equality",
+			prettyValue(received),
+			ctx.inverted and " not " or " ",
+			prettyValue(item)
+		),
+	}
+end
 
 ---@type lest.Matcher
 local function toMatchObject(ctx, received, object) end
 
-return {}
+return {
+	toHaveLength = toHaveLength,
+	toContain = toContain,
+	toContainEqual = toContainEqual,
+}
---
+++ src/matchers/tables.test.lua
@@ -0,0 +1,238 @@
+local matchers = require("src.matchers.tables")
+local assertMatcher = require("src.asserts.matchers")
+local prettyValue = require("src.utils.prettyValue")
+
+describe("table matchers", function()
+	local CONTEXT = {
+		inverted = false,
+	}
+
+	local INVERTED_CONTEXT = {
+		inverted = true,
+	}
+
+	describe("toHaveLength", function()
+		it(
+			"should pass when the received object has the expected length",
+			function()
+				-- Given
+				local testArray = { 1, 2, 3 }
+				local testTable = { a = 1, b = 2, c = 3 }
+				local testString = "abcde"
+
+				-- When
+				local resultArray = matchers.toHaveLength(CONTEXT, testArray, 3)
+				local resultTable = matchers.toHaveLength(CONTEXT, testTable, 0)
+				local resultString =
+					matchers.toHaveLength(CONTEXT, testString, 5)
+
+				-- Then
+				assertMatcher.passed(resultArray)
+				assertMatcher.passed(resultTable)
+				assertMatcher.passed(resultString)
+			end
+		)
+
+		it(
+			"should fail when the received object doesn't has the expected length",
+			function()
+				-- Given
+				local testArray = { 2, 3, 4 }
+
+				-- When
+				local resultArray = matchers.toHaveLength(CONTEXT, testArray, 4)
+
+				-- Then
+				assertMatcher.failed(resultArray)
+				assertMatcher.hasMessage(
+					resultArray,
+					("Expected %s to have the length of %d"):format(
+						prettyValue(testArray),
+						4
+					)
+				)
+			end
+		)
+
+		it("should have an inverted message", function()
+			-- Given
+			local testArray = { 2, 3, 4 }
+
+			-- When
+			local resultArray =
+				matchers.toHaveLength(INVERTED_CONTEXT, testArray, 4)
+
+			-- Then
+			assertMatcher.hasMessage(
+				resultArray,
+				("Expected %s to not have the length of %d"):format(
+					prettyValue(testArray),
+					4
+				)
+			)
+		end)
+	end)
+
+	describe("toContain", function()
+		describe("should pass", function()
+			test(
+				"when the received object contains the expected item",
+				function()
+					-- Given
+					local testArray = { 1, 2, "foo" }
+					local testItem = "foo"
+
+					-- When
+					local result =
+						matchers.toContain(CONTEXT, testArray, testItem)
+
+					-- Then
+					assertMatcher.passed(result)
+				end
+			)
+
+			test(
+				"when the received object is a string and it contains the expected substring",
+				function()
+					-- Given
+					local testString = "foobar"
+					local testItem = "foo"
+
+					-- When
+					local result =
+						matchers.toContain(CONTEXT, testString, testItem)
+
+					-- Then
+					assertMatcher.passed(result)
+				end
+			)
+		end)
+
+		describe("should fail", function()
+			test(
+				"when the received object doesn't contain the expected item",
+				function()
+					-- Given
+					local testArray = { 1, 2, "foo" }
+					local testItem = "quux"
+
+					-- When
+					local result =
+						matchers.toContain(CONTEXT, testArray, testItem)
+
+					-- Then
+					assertMatcher.failed(result)
+					assertMatcher.hasMessage(
+						result,
+						("Expected %s to contain %s"):format(
+							prettyValue(testArray),
+							prettyValue(testItem)
+						)
+					)
+				end
+			)
+
+			test(
+				"when the received object is a string and it doesn't contains the expected substring",
+				function()
+					-- Given
+					local testString = "foobar"
+					local testItem = "quux"
+
+					-- When
+					local result =
+						matchers.toContain(CONTEXT, testString, testItem)
+
+					-- Then
+					assertMatcher.failed(result)
+					assertMatcher.hasMessage(
+						result,
+						("Expected %s to contain %s"):format(
+							prettyValue(testString),
+							prettyValue(testItem)
+						)
+					)
+				end
+			)
+		end)
+
+		it("should have an inverted message", function()
+			-- Given
+			local testArray = { 1, 10 }
+			local testItem = 10
+
+			-- When
+			local result =
+				matchers.toContain(INVERTED_CONTEXT, testArray, testItem)
+
+			-- Given
+			assertMatcher.hasMessage(
+				result,
+				("Expected %s to not contain %s"):format(
+					prettyValue(testArray),
+					prettyValue(testItem)
+				)
+			)
+		end)
+	end)
+
+	describe("toContainEqual", function()
+		it(
+			"should pass when the received object contains the expected item with deep equality",
+			function()
+				-- Given
+				local testArray = { 1, { hello = 10, hi = { turn = 10 } }, 2 }
+				local testItem = { hello = 10, hi = { turn = 10 } }
+
+				-- When
+				local result =
+					matchers.toContainEqual(CONTEXT, testArray, testItem)
+
+				-- Then
+				assertMatcher.passed(result)
+			end
+		)
+
+		it(
+			"should fail when the received object doesn't contains the expected item with deep equality",
+			function()
+				-- Given
+				local testArray = { 1, { hello = 10, hi = { turn = 10 } }, 2 }
+				local testItem = { foo = 10, bar = { quux = 10 } }
+
+				-- When
+				local result =
+					matchers.toContainEqual(CONTEXT, testArray, testItem)
+
+				-- Then
+				assertMatcher.failed(result)
+				assertMatcher.hasMessage(
+					result,
+					("Expected %s to contain %s with deep equality"):format(
+						prettyValue(testArray),
+						prettyValue(testItem)
+					)
+				)
+			end
+		)
+
+		it("should have an inverted message", function()
+			-- Given
+			local testArray = { 1, 30, 10 }
+			local testItem = 30
+
+			-- When
+			local result =
+				matchers.toContainEqual(INVERTED_CONTEXT, testArray, testItem)
+
+			-- Then
+			assertMatcher.hasMessage(
+				result,
+				("Expected %s to not contain %s with deep equality"):format(
+					prettyValue(testArray),
+					prettyValue(testItem)
+				)
+			)
+		end)
+	end)
+end)
