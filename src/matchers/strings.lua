b4ac8a06bd06ee5374ba0b373abd90309fe14608
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-02-23 19:46:30 -0800

    Add `toMatch` and the test suite.

--- src/matchers/strings.lua
+++ src/matchers/strings.lua
@@ -1,4 +1,22 @@
----@type lest.Matcher
-local function toMatch(ctx, received, pattern) end
+local prettyValue = require("src.utils.prettyValue")
+local assertType = require("src.asserts.type")
 
-return {}
+---@type lest.Matcher
+local function toMatch(ctx, received, pattern)
+	assertType(received, "string")
+	assertType(pattern, "string")
+
+	return {
+		pass = received:match(pattern) ~= nil,
+		message = string.format(
+			"Expected %s to%smatch %s",
+			prettyValue(received),
+			ctx.inverted and " not " or " ",
+			prettyValue(pattern)
+		),
+	}
+end
+
+return {
+	toMatch = toMatch,
+}
