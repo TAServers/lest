b679bebd77c86aa54ec72d36adaee4e9f1d59aca
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-03-13 13:15:53 -0700

    LEST-30 - String matchers (#17)

--- src/matchers/strings.lua
+++ src/matchers/strings.lua
@@ -1,4 +1,21 @@
----@type lest.Matcher
-local function toMatch(ctx, received, pattern) end
+local prettyValue = require("src.utils.prettyValue")
+local assertType = require("src.asserts.type")
 
-return {}
+---@type lest.Matcher
+local function toMatch(ctx, received, pattern)
+	assertType(pattern, "string")
+
+	return {
+		pass = type(received) == "string" and received:match(pattern) ~= nil,
+		message = string.format(
+			"Expected %s to%smatch %s",
+			prettyValue(received),
+			ctx.inverted and " not " or " ",
+			prettyValue(pattern)
+		),
+	}
+end
+
+return {
+	toMatch = toMatch,
+}
