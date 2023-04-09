cbab0b5b717b0985c47442c7cb8e4e4a49749a4a
Derpius <49565664+Derpius@users.noreply.github.com>
2023-04-09 17:28:04 +0100

    Use luamin fork which works in CI

--- scripts/install.lua
+++ scripts/install.lua
@@ -1,2 +1,5 @@
-local success = os.execute([[npm install -g luabundler luamin]])
+-- Fork of luamin used as upstream doesn't work in CI due to depending on a valid TTY
+local success = os.execute(
+	[[npm install -g luabundler https://github.com/hsandt/luamin.git#develop]]
+)
 os.exit(success and 0 or 1)
