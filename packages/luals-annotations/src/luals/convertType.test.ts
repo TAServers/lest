b427dfa0812944589c41525811f0a7fc336fdf42
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-06-27 16:26:09 -0700

    feat: add `convertType` to transform doc types to luals types

--- packages/luals-annotations/src/luals/convertType.test.ts
+++ packages/luals-annotations/src/luals/convertType.test.ts
@@ -31,4 +31,121 @@ describe("convertType", () => {
 		// Assert
 		expect(luaLSType).toEqual("lest.MockResult[]");
 	});
+
+	it("should handle untyped arrays of arrays", () => {
+		// Arrange
+		const testProperty: Docs.ArrayProperty = {
+			name: "test",
+			type: "array",
+			items: {
+				type: "array",
+				items: "any",
+			},
+		};
+
+		// Act
+		const luaLSType = convertType(testProperty);
+
+		// Assert
+		expect(luaLSType).toEqual("any[][]");
+	});
+
+	it("should handle specified function types", () => {
+		// Arrange
+		const testProperty: Docs.FunctionProperty = {
+			name: "test",
+			type: "function",
+			parameters: [
+				{
+					name: "a",
+					type: "string",
+				},
+			],
+			returns: [
+				{
+					name: "b",
+					type: "number",
+				},
+			],
+		};
+
+		// Act
+		const luaLSType = convertType(testProperty);
+
+		// Assert
+		expect(luaLSType).toEqual("fun(a:string):number");
+	});
+
+	it("should handle unspecified function types", () => {
+		// Arrange
+		const testProperty: Docs.Property = {
+			name: "test",
+			type: "function",
+		};
+
+		// Act
+		const luaLSType = convertType(testProperty);
+
+		// Assert
+		expect(luaLSType).toEqual("function");
+	});
+
+	it("should handle multiple string literal types", () => {
+		// These are actually valid in LuaLS. So we don't need to do anything special.
+		// Arrange
+		const testProperty: Docs.Property = {
+			name: "test",
+			type: '"test" | "test2"',
+		};
+
+		// Act
+		const luaLSType = convertType(testProperty);
+
+		// Assert
+		expect(luaLSType).toEqual('"test" | "test2"');
+	});
+
+	it("should handle tables", () => {
+		// Arrange
+		const testProperty: Docs.TableProperty = {
+			name: "test",
+			type: "table",
+			fields: [
+				{
+					name: "foo",
+					type: "string",
+				},
+				{
+					name: "bar",
+					type: "number",
+				},
+				{
+					name: "baz",
+					type: "boolean",
+				},
+				{
+					name: "qux",
+					type: "function",
+					parameters: [
+						{
+							name: "a",
+							type: "string",
+						},
+					],
+					returns: [
+						{
+							name: "b",
+							type: "number",
+						},
+					],
+				} as Docs.FunctionProperty,
+			],
+		};
+
+		// Act
+		const luaLSType = convertType(testProperty);
+
+		// Assert
+		expect(luaLSType).toEqual("{foo:string,bar:number,baz:boolean,qux:fun(a:string):number}");
+	});
 });
--- packages/luals-annotations/src/luals/convertType.ts
+++ packages/luals-annotations/src/luals/convertType.ts
@@ -1,5 +1,26 @@
 import { Docs } from "../json-docs";
 
 export default function convertType(docType: Docs.Property): string {
-	return "";
+	if (Docs.isFunctionProperty(docType)) {
+		if (!docType.parameters && !docType.returns) {
+			return "function";
+		}
+
+		const params = docType.parameters ?? [];
+		const returns = docType.returns ?? [];
+		const paramList = params.map((param) => `${param.name}:${convertType(param)}`).join(",");
+		const returnList = returns.map((ret) => `${convertType(ret)}`).join(",");
+
+		return `fun(${paramList}):${returnList}`;
+	} else if (Docs.isArrayProperty(docType)) {
+		if (typeof docType.items === "string") {
+			return `${docType.items}[]`;
+		} else {
+			return `${convertType(docType.items)}[]`;
+		}
+	} else if (Docs.isTableProperty(docType)) {
+		return `{${docType.fields.map((field) => `${field.name}:${convertType(field)}`).join(",")}}`;
+	}
+
+	return docType.type;
 }
