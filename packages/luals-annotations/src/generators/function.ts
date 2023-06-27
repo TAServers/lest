e24f5e619ceac7297b0cba909e39a66d9a960377
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-06-26 21:24:48 -0700

    feat: add function annotation generator

---
+++ packages/luals-annotations/src/generators/function.ts
@@ -0,0 +1,31 @@
+import { Docs } from "@/json-docs";
+
+function makeSignature(name: string, params: Docs.Property[]): string {
+	return `function ${name}(${params.map((param) => param.name).join(",")})`;
+}
+
+export default function makeFunctionAnnotation(func: Docs.Function): string {
+	let descriptionAnnotation = "";
+	if (typeof func.description === "string") {
+		descriptionAnnotation = `--- ${func.description}`;
+	} else {
+		descriptionAnnotation = func.description.map((descLine) => `--- ${descLine}`).join("\n");
+	}
+
+	const params = func.parameters || [];
+	const returns = func.returns || [];
+	const signature = makeSignature(func.name, params);
+
+	let paramAnnotation = params.map((param) => `---@param ${param.name} ${param.type}`).join("\n");
+	let returnAnnotation = returns.map((ret) => `---@return ${ret.type} ${ret.name}`).join("\n");
+
+	if (!func.aliases) {
+		return [descriptionAnnotation, paramAnnotation, returnAnnotation, signature].join("\n");
+	} else {
+		return [func.name, ...func.aliases]
+			.map((name) =>
+				[descriptionAnnotation, paramAnnotation, returnAnnotation, makeSignature(name, params)].join("\n")
+			)
+			.join("\n\n");
+	}
+}
---
+++ packages/luals-annotations/src/json-docs/index.ts
@@ -0,0 +1,93 @@
+import * as afterAll from "@lest/docs/functions/afterAll.json";
+import * as afterEach from "@lest/docs/functions/afterEach.json";
+import * as beforeAll from "@lest/docs/functions/beforeAll.json";
+import * as beforeEach from "@lest/docs/functions/beforeEach.json";
+import * as describe_each from "@lest/docs/functions/describe-each.json";
+import * as describe from "@lest/docs/functions/describe.json";
+import * as expect from "@lest/docs/functions/expect.json";
+import * as fn from "@lest/docs/functions/fn.json";
+import * as test_each from "@lest/docs/functions/test-each.json";
+import * as test from "@lest/docs/functions/test.json";
+import * as toBe from "@lest/docs/matchers/symmetric/toBe.json";
+import * as toHaveBeenCalled from "@lest/docs/matchers/symmetric/toHaveBeenCalled.json";
+import * as toHaveBeenCalledTimes from "@lest/docs/matchers/symmetric/toHaveBeenCalledTimes.json";
+import * as toHaveBeenCalledWith from "@lest/docs/matchers/symmetric/toHaveBeenCalledWith.json";
+import * as toHaveBeenLastCalledWith from "@lest/docs/matchers/symmetric/toHaveBeenLastCalledWith.json";
+import * as toHaveBeenNthCalledWith from "@lest/docs/matchers/symmetric/toHaveBeenNthCalledWith.json";
+import * as toHaveLastReturnedWith from "@lest/docs/matchers/symmetric/toHaveLastReturnedWith.json";
+import * as toHaveNthReturnedWith from "@lest/docs/matchers/symmetric/toHaveNthReturnedWith.json";
+import * as toHaveReturned from "@lest/docs/matchers/symmetric/toHaveReturned.json";
+import * as toHaveReturnedTimes from "@lest/docs/matchers/symmetric/toHaveReturnedTimes.json";
+import * as toHaveReturnedWith from "@lest/docs/matchers/symmetric/toHaveReturnedWith.json";
+import * as mock_result from "@lest/docs/types/mock-result.json";
+import * as mock from "@lest/docs/types/mock.json";
+
+export namespace Docs {
+	export interface Function {
+		name: string;
+		aliases?: string[];
+		description?: string | string[];
+		parameters?: Property[];
+		returns?: Property[];
+	}
+
+	export interface Class {
+		name: string;
+		description?: string | string[];
+		fields?: Property[];
+		methods?: Function[];
+	}
+
+	export interface Property {
+		type: string;
+		name?: string;
+		description?: string | string[];
+		optional?: boolean;
+	}
+
+	export interface FunctionProperty extends Property {
+		type: "function";
+		static?: boolean;
+		parameters?: Property[];
+		returns?: Property[];
+	}
+
+	export type ArrayItems = string | { type: "array"; items: ArrayItems };
+
+	export interface ArrayProperty extends Property {
+		type: "array";
+		items: ArrayItems;
+	}
+
+	export interface TableProperty extends Property {
+		type: "table";
+		fields: Property[];
+	}
+}
+
+export const functionsList: Docs.Function[] = [
+	afterAll,
+	afterEach,
+	beforeAll,
+	beforeEach,
+	describe_each,
+	describe,
+	expect,
+	test_each,
+	test,
+	fn,
+];
+export const matcherFunctions: Docs.Function[] = [
+	toBe,
+	toHaveBeenCalled,
+	toHaveBeenCalledTimes,
+	toHaveBeenCalledWith,
+	toHaveBeenLastCalledWith,
+	toHaveBeenNthCalledWith,
+	toHaveLastReturnedWith,
+	toHaveNthReturnedWith,
+	toHaveReturned,
+	toHaveReturnedTimes,
+	toHaveReturnedWith,
+];
+export const classList: Docs.Class[] = [mock, mock_result];
