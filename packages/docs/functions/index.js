20cdeb507020d6bbc410c60794665ed8590063a6
Derpius <49565664+Derpius@users.noreply.github.com>
2023-07-01 21:36:59 +0100

    docs: add folder exports

---
+++ packages/docs/functions/index.js
@@ -0,0 +1,10 @@
+export { default as afterAll } from "./afterAll.json";
+export { default as afterEach } from "./afterEach.json";
+export { default as beforeAll } from "./beforeAll.json";
+export { default as beforeEach } from "./beforeEach.json";
+export { default as describe } from "./describe.json";
+export { default as describeEach } from "./describe-each.json";
+export { default as expect } from "./expect.json";
+export { default as lestFn } from "./fn.json";
+export { default as test } from "./test.json";
+export { default as testEach } from "./test-each.json";
