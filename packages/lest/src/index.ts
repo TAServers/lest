d907edd1263a3fee9ea08bf00654f4b3b971fb08
Derpius <49565664+Derpius@users.noreply.github.com>
2023-07-02 20:20:22 +0100

    feat: run lest from node wrapper

--- packages/lest/src/index.ts
+++ packages/lest/src/index.ts
@@ -1,13 +1,26 @@
 import minimist from "minimist";
 import path from "path";
 import { findLuaExecutable } from "./helpers";
+import { spawn } from "child_process";
 
 const { luaCommand } = minimist(process.argv.slice(2));
 
-const getLestLuaPath = () => path.join(__dirname, "lest.lua");
+const getLestLuaPath = () => path.join(__dirname, "lua", "lest.lua");
 
 findLuaExecutable(luaCommand).then((executablePath) => {
 	const scriptPath = getLestLuaPath();
 
-	console.log(`${executablePath} ${scriptPath}`);
+	const lestProcess = spawn(executablePath, [scriptPath, ...process.argv.slice(2)]);
+
+	lestProcess.stdout.on("data", (data: Buffer) => {
+		process.stdout.write(data);
+	});
+
+	lestProcess.stderr.on("data", (data: Buffer) => {
+		process.stderr.write(data);
+	});
+
+	lestProcess.on("error", (error) => {
+		console.error(`Failed to run command:\n${error.message}`);
+	});
 });
