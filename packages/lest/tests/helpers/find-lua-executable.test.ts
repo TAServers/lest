8c2d6d8491e2852e500aad32eb16f2b278291546
Derpius <49565664+Derpius@users.noreply.github.com>
2023-07-02 19:44:28 +0100

    chore: configure jest

---
+++ packages/lest/tests/helpers/find-lua-executable.test.ts
@@ -0,0 +1,11 @@
+test.todo("checks all lua executable names in order");
+
+test.todo("returns the first found path");
+
+test.todo("throws an error if no path could be found");
+
+describe("with luaCommand", () => {
+	test.todo("returns the resolved path if found");
+
+	test.todo("throws an error if the given command could not be resolved");
+});
