591bc351ef879b6ed5abab0c0928e6557be7e072
Derpius <49565664+Derpius@users.noreply.github.com>
2023-07-06 21:34:03 +0100

    refactor: clean up imports

--- packages/docs/src/types/index.ts
+++ packages/docs/src/types/index.ts
@@ -1,6 +1,6 @@
 import { Class } from "../interfaces";
-import * as _mock from "./mock.json";
-import * as _mockResult from "./mock-result.json";
+import _mock from "./mock.json";
+import _mockResult from "./mock-result.json";
 
 export const mock: Class = _mock;
 export const mockResult: Class = _mockResult;
