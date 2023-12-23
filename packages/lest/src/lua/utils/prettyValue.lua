fb612bb4e2fb59594a9b46dc86990a6d7a4ecee0
Derpius <49565664+Derpius@users.noreply.github.com>
2023-12-23 12:42:12 +0000

    Upgrade prettyValue to preview tables

--- packages/lest/src/lua/utils/prettyValue.lua
+++ packages/lest/src/lua/utils/prettyValue.lua
@@ -1,7 +1,39 @@
---- Formats a value to look prettier
+local MAX_TABLE_FIELDS = 3
+
+--- Iterator function which returns elements in numeric then lexicographic order
+---@param tbl table
+---@return fun(): any, any
+local function sortedPairs(tbl)
+	local keys = {}
+	for key in pairs(tbl) do
+		table.insert(keys, key)
+	end
+
+	table.sort(keys, function(a, b)
+		local aIsNumber = type(a) == "number"
+		local bIsNumber = type(b) == "number"
+
+		if aIsNumber then
+			return not bIsNumber or a < b
+		end
+
+		return not bIsNumber and tostring(a) < tostring(b)
+	end)
+
+	local i = 1
+	return function()
+		local key = keys[i]
+		if key ~= nil then
+			i = i + 1
+			return key, tbl[key]
+		end
+	end
+end
+
+--- Renders a primitive value, or table with tostring
 ---@param value any
 ---@return string
-return function(value)
+local function renderPrimitive(value)
 	if type(value) == "string" then
 		return '"' .. value .. '"'
 	end
@@ -19,4 +51,61 @@ return function(value)
 	end
 
 	return tostring(value)
+end
+
+--- Renders an inline truncated table
+---@param tbl table
+---@return string
+local function renderTable(tbl)
+	local renderedFields = {}
+	local totalFields = 0
+
+	for key, value in sortedPairs(tbl) do
+		if #renderedFields < MAX_TABLE_FIELDS then
+			if type(key) == "number" then
+				table.insert(renderedFields, renderPrimitive(value))
+			elseif
+				type(key) == "string" and string.match(key, "^[_%a][_%a%d]*$")
+			then
+				table.insert(
+					renderedFields,
+					string.format("%s = %s", key, renderPrimitive(value))
+				)
+			else
+				table.insert(
+					renderedFields,
+					string.format(
+						"[%s] = %s",
+						renderPrimitive(key),
+						renderPrimitive(value)
+					)
+				)
+			end
+		end
+
+		totalFields = totalFields + 1
+	end
+
+	if #renderedFields < totalFields then
+		table.insert(
+			renderedFields,
+			string.format("...%d more", totalFields - #renderedFields)
+		)
+	end
+
+	return string.format("{ %s }", table.concat(renderedFields, ", "))
+end
+
+--- Formats a value to look prettier
+---@param value any
+---@return string
+return function(value)
+	if
+		type(value) == "table"
+		and string.match(tostring(value), "table: [%a%d]+")
+	then
+		return renderTable(value)
+	end
+
+	return renderPrimitive(value)
 end
