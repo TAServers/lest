4e953ca187d25b15195493104e851f17d3079aa9
Derpius <49565664+Derpius@users.noreply.github.com>
2023-12-23 11:48:51 +0000

    Add simple diff renderer with no highlighting

---
+++ packages/lest/src/lua/utils/render-diff.lua
@@ -0,0 +1,153 @@
+local prettyValue = require("utils.prettyValue")
+
+--- Renders a field in the diff of a table
+---@param prefix " "|"-"|"+"
+---@param indentation string
+---@param key any
+---@param value any
+---@param shouldPrettyPrintValue? boolean # Defaults to true
+---@return string
+local function renderTableField(
+	prefix,
+	indentation,
+	key,
+	value,
+	shouldPrettyPrintValue
+)
+	if shouldPrettyPrintValue == nil then
+		shouldPrettyPrintValue = true
+	end
+
+	return string.format(
+		"%s %s[%s] = %s,\n",
+		prefix,
+		indentation,
+		prettyValue(key),
+		shouldPrettyPrintValue and prettyValue(value) or value
+	)
+end
+
+--- Renders a difference between two tables with optional indentation and highlighting
+---@param expectedTable table
+---@param receivedTable table
+---@param withColour boolean
+---@param indentation? string
+---@param visitedTables? table<table, boolean>
+---@return table
+local function renderTableDiff(
+	expectedTable,
+	receivedTable,
+	withColour,
+	indentation,
+	visitedTables
+)
+	indentation = indentation or "  "
+	visitedTables = visitedTables or {}
+	local rendered = "Table {\n"
+	local expected = 0
+	local received = 0
+
+	visitedTables[expectedTable] = true
+	visitedTables[receivedTable] = true
+
+	for key, expectedValue in pairs(expectedTable) do
+		local receivedValue = receivedTable[key]
+
+		if expectedValue == receivedValue then
+			rendered = rendered
+				.. renderTableField(" ", indentation, key, expectedValue)
+		elseif receivedValue == nil then
+			rendered = rendered
+				.. renderTableField("-", indentation, key, expectedValue)
+			expected = expected + 1
+		elseif visitedTables[expectedValue] or visitedTables[receivedValue] then
+			rendered = rendered
+				.. renderTableField(
+					"-",
+					indentation,
+					key,
+					visitedTables[expectedValue] and "-- Circular reference --"
+						or expectedValue,
+					not visitedTables[expectedValue]
+				)
+				.. renderTableField(
+					"+",
+					indentation,
+					key,
+					visitedTables[receivedValue] and "-- Circular reference --"
+						or receivedValue,
+					not visitedTables[receivedValue]
+				)
+			expected = expected + 1
+			received = received + 1
+		elseif
+			type(expectedValue) == "table"
+			and type(receivedValue) == "table"
+		then
+			local valueDiff = renderTableDiff(
+				expectedValue,
+				receivedValue,
+				withColour,
+				indentation .. "  ",
+				visitedTables
+			)
+
+			rendered = rendered
+				.. renderTableField(
+					" ",
+					indentation,
+					key,
+					valueDiff.rendered,
+					false
+				)
+			expected = expected + valueDiff.expected
+			received = received + valueDiff.received
+		else
+			rendered = rendered
+				.. renderTableField("-", indentation, key, expectedValue)
+				.. renderTableField("+", indentation, key, receivedValue)
+			expected = expected + 1
+			received = received + 1
+		end
+	end
+
+	for key, value in pairs(receivedTable) do
+		if expectedTable[key] == nil then
+			rendered = rendered
+				.. renderTableField("+", indentation, key, value)
+			received = received + 1
+		end
+	end
+
+	return {
+		rendered = rendered .. indentation .. "}",
+		expected = expected,
+		received = received,
+	}
+end
+
+--- Renders the difference between two values with optional ANSI colour highlighting
+---@param expectedValue any
+---@param receivedValue any
+---@param withColour? boolean # Defaults to false
+---@return string
+local function renderDiff(expectedValue, receivedValue, withColour)
+	if type(expectedValue) ~= "table" or type(receivedValue) ~= "table" then
+		return string.format(
+			"Expected: %s\nReceived: %s",
+			prettyValue(expectedValue),
+			prettyValue(receivedValue)
+		)
+	end
+
+	local diff =
+		renderTableDiff(expectedValue, receivedValue, not not withColour)
+	return string.format(
+		"- Expected  - %d\n+ Received  + %d\n\n  %s",
+		diff.expected,
+		diff.received,
+		diff.rendered
+	)
+end
+
+return renderDiff
---
+++ packages/lest/src/lua/utils/tests/render-diff.test.lua
@@ -0,0 +1,148 @@
+local renderDiff = require("utils.render-diff")
+
+test.each({
+	{
+		"primitive before and after",
+		123,
+		"456",
+		[[Expected: 123
+Received: "456"]],
+	},
+	{
+		"primitive before, table after",
+		123,
+		{ 1, 2, 3 },
+		[[Expected: 123
+Received: { 1, 2, 3 }]],
+	},
+	{
+		"table before, primitive after",
+		{ 1, 2, 3 },
+		123,
+		[[Expected: { 1, 2, 3 }
+Received: 123]],
+	},
+	{
+		"non-recursive equal tables",
+		{ foo = 1 },
+		{ foo = 1 },
+		[[- Expected  - 0
++ Received  + 0
+
+  Table {
+    ["foo"] = 1,
+  }]],
+	},
+	{
+		"non-recursive tables with different value",
+		{ foo = 1 },
+		{ foo = 2 },
+		[[- Expected  - 1
++ Received  + 1
+
+  Table {
+-   ["foo"] = 1,
++   ["foo"] = 2,
+  }]],
+	},
+	{
+		"non-recursive tables with different field",
+		{ foo = 1 },
+		{ bar = 1 },
+		[[- Expected  - 1
++ Received  + 1
+
+  Table {
+-   ["foo"] = 1,
++   ["bar"] = 1,
+  }]],
+	},
+	{
+		"non-recursive tables with missing field",
+		{ foo = 1 },
+		{},
+		[[- Expected  - 1
++ Received  + 0
+
+  Table {
+-   ["foo"] = 1,
+  }]],
+	},
+	{
+		"recursive tables",
+		{ { foo = 1 }, { bar = 2 }, { baz = 3 } },
+		{ { foo = 1 }, { bar = 3 }, { biz = 3 } },
+		[[- Expected  - 2
++ Received  + 2
+
+  Table {
+    [1] = Table {
+      ["foo"] = 1,
+    },
+    [2] = Table {
+-     ["bar"] = 2,
++     ["bar"] = 3,
+    },
+    [3] = Table {
+-     ["baz"] = 3,
++     ["biz"] = 3,
+    },
+  }]],
+	},
+})(
+	"correctly renders %s",
+	function(_, expectedValue, receivedValue, expectedDiff)
+		local rendered = renderDiff(expectedValue, receivedValue, false)
+
+		expect(rendered).toBe(expectedDiff)
+	end
+)
+
+test("handles circular reference in expected table", function()
+	local expectedValue = {}
+	expectedValue.bar = expectedValue
+	local receivedValue = { bar = "5" }
+
+	local rendered = renderDiff(expectedValue, receivedValue, false)
+
+	expect(rendered).toBe([[- Expected  - 1
++ Received  + 1
+
+  Table {
+-   ["bar"] = -- Circular reference --,
++   ["bar"] = "5",
+  }]])
+end)
+
+test("handles circular reference in received table", function()
+	local expectedValue = { bar = "5" }
+	local receivedValue = {}
+	receivedValue.bar = receivedValue
+
+	local rendered = renderDiff(expectedValue, receivedValue, false)
+
+	expect(rendered).toBe([[- Expected  - 1
++ Received  + 1
+
+  Table {
+-   ["bar"] = "5",
++   ["bar"] = -- Circular reference --,
+  }]])
+end)
+
+test("handles circular reference in both tables", function()
+	local expectedValue = {}
+	expectedValue.bar = expectedValue
+	local receivedValue = {}
+	receivedValue.bar = receivedValue
+
+	local rendered = renderDiff(expectedValue, receivedValue, false)
+
+	expect(rendered).toBe([[- Expected  - 1
++ Received  + 1
+
+  Table {
+-   ["bar"] = -- Circular reference --,
++   ["bar"] = -- Circular reference --,
+  }]])
+end)
